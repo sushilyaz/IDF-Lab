d8cbd8e8176c539130aaff159a8ef01a859adcf1
suhoi <suhoio2k00@gmail.com>
2024-03-23 20:48:12 +0300

    Initial commit

---
+++ src/test/java/org/example/idflab/IdfLabApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.idflab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdfLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
